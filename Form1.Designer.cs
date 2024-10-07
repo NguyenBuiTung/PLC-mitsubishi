c8fca0b74553eb5b4bc7d925fdb13eafb1c01d74
Tung <nguyenbuitung4@gmail.com>
2024-10-07 15:00:23 +0700

    ff

---
+++ Form1.Designer.cs
@@ -0,0 +1,144 @@
+﻿
+namespace Connect_With_PLC_Mitsubishi
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btConnect = new System.Windows.Forms.Button();
+            this.btDisconnect = new System.Windows.Forms.Button();
+            this.txtData = new System.Windows.Forms.TextBox();
+            this.btnRead = new System.Windows.Forms.Button();
+            this.btnWrite = new System.Windows.Forms.Button();
+            this.btnReadFloat = new System.Windows.Forms.Button();
+            this.btnWriteFloat = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btConnect
+            // 
+            this.btConnect.BackColor = System.Drawing.Color.Lime;
+            this.btConnect.Location = new System.Drawing.Point(69, 68);
+            this.btConnect.Margin = new System.Windows.Forms.Padding(2);
+            this.btConnect.Name = "btConnect";
+            this.btConnect.Size = new System.Drawing.Size(137, 63);
+            this.btConnect.TabIndex = 0;
+            this.btConnect.Text = "Connect\r\n";
+            this.btConnect.UseVisualStyleBackColor = false;
+            this.btConnect.Click += new System.EventHandler(this.btConnect_Click);
+            // 
+            // btDisconnect
+            // 
+            this.btDisconnect.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.btDisconnect.Location = new System.Drawing.Point(69, 179);
+            this.btDisconnect.Margin = new System.Windows.Forms.Padding(2);
+            this.btDisconnect.Name = "btDisconnect";
+            this.btDisconnect.Size = new System.Drawing.Size(137, 63);
+            this.btDisconnect.TabIndex = 1;
+            this.btDisconnect.Text = "Disconnect\r\n";
+            this.btDisconnect.UseVisualStyleBackColor = false;
+            this.btDisconnect.Click += new System.EventHandler(this.btDisconnect_Click);
+            // 
+            // txtData
+            // 
+            this.txtData.Location = new System.Drawing.Point(287, 90);
+            this.txtData.Name = "txtData";
+            this.txtData.Size = new System.Drawing.Size(100, 20);
+            this.txtData.TabIndex = 2;
+            // 
+            // btnRead
+            // 
+            this.btnRead.Location = new System.Drawing.Point(287, 132);
+            this.btnRead.Name = "btnRead";
+            this.btnRead.Size = new System.Drawing.Size(100, 36);
+            this.btnRead.TabIndex = 3;
+            this.btnRead.Text = "Đọc";
+            this.btnRead.UseVisualStyleBackColor = true;
+            this.btnRead.Click += new System.EventHandler(this.btnRead_Click);
+            // 
+            // btnWrite
+            // 
+            this.btnWrite.Location = new System.Drawing.Point(287, 179);
+            this.btnWrite.Name = "btnWrite";
+            this.btnWrite.Size = new System.Drawing.Size(100, 36);
+            this.btnWrite.TabIndex = 4;
+            this.btnWrite.Text = "Ghi";
+            this.btnWrite.UseVisualStyleBackColor = true;
+            this.btnWrite.Click += new System.EventHandler(this.btnWrite_Click);
+            // 
+            // btnReadFloat
+            // 
+            this.btnReadFloat.Location = new System.Drawing.Point(421, 132);
+            this.btnReadFloat.Name = "btnReadFloat";
+            this.btnReadFloat.Size = new System.Drawing.Size(100, 36);
+            this.btnReadFloat.TabIndex = 5;
+            this.btnReadFloat.Text = "Đọc số thực";
+            this.btnReadFloat.UseVisualStyleBackColor = true;
+            this.btnReadFloat.Click += new System.EventHandler(this.btnReadFloat_Click);
+            // 
+            // btnWriteFloat
+            // 
+            this.btnWriteFloat.Location = new System.Drawing.Point(421, 179);
+            this.btnWriteFloat.Name = "btnWriteFloat";
+            this.btnWriteFloat.Size = new System.Drawing.Size(100, 36);
+            this.btnWriteFloat.TabIndex = 6;
+            this.btnWriteFloat.Text = "Ghi số thực";
+            this.btnWriteFloat.UseVisualStyleBackColor = true;
+            this.btnWriteFloat.Click += new System.EventHandler(this.btnWriteFloat_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(533, 292);
+            this.Controls.Add(this.btnWriteFloat);
+            this.Controls.Add(this.btnReadFloat);
+            this.Controls.Add(this.btnWrite);
+            this.Controls.Add(this.btnRead);
+            this.Controls.Add(this.txtData);
+            this.Controls.Add(this.btDisconnect);
+            this.Controls.Add(this.btConnect);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.Text = "Connect With PLC";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btConnect;
+        private System.Windows.Forms.Button btDisconnect;
+        private System.Windows.Forms.TextBox txtData;
+        private System.Windows.Forms.Button btnRead;
+        private System.Windows.Forms.Button btnWrite;
+        private System.Windows.Forms.Button btnReadFloat;
+        private System.Windows.Forms.Button btnWriteFloat;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.Windows.Forms;
+using ActUtlTypeLib; // Thư viện cần thiết để làm việc với PLC Mitsubishi
+
+namespace Connect_With_PLC_Mitsubishi
+{
+    public partial class Form1 : Form
+    {
+        public ActUtlType plc = new ActUtlType(); // Đối tượng ActUtlType để giao tiếp với PLC Mitsubishi
+
+        public Form1()
+        {
+            InitializeComponent(); // Khởi tạo các thành phần giao diện người dùng
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Đây là sự kiện tải form. Bạn có thể thêm mã khởi tạo ở đây nếu cần.
+        }
+        private bool IsConnected()
+        {
+            int testValue;
+            int result = plc.GetDevice("D0", out testValue); // Đọc dữ liệu từ địa chỉ "D0" để kiểm tra kết nối
+
+            // Nếu phương thức GetDevice trả về 0, kết nối còn hoạt động; ngược lại, kết nối đã bị ngắt
+            return result == 0;
+        }
+
+        private void btConnect_Click(object sender, EventArgs e)
+        {
+            plc.ActLogicalStationNumber = 2; // Thiết lập số trạm logic của PLC (ví dụ: 2)
+            short connectResult = (short)plc.Open(); // Mở kết nối với PLC
+            if (connectResult == 0)
+            {
+                MessageBox.Show("Connect with PLC Successful", "Message", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                MessageBox.Show("Connect with PLC Error", "Message", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btDisconnect_Click(object sender, EventArgs e)
+        {
+            if (IsConnected())
+            {
+                plc.Close(); // Đóng kết nối với PLC nếu đang kết nối
+                MessageBox.Show("Disconnected from PLC", "Message", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                MessageBox.Show("PLC is not connected", "Message", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        private void btnRead_Click(object sender, EventArgs e)
+        {
+            if (!IsConnected()) // Kiểm tra kết nối trước khi đọc dữ liệu
+            {
+                MessageBox.Show("PLC is not connected", "Message", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            int readData = 0; // Biến để lưu dữ liệu đọc được
+            int readResult = plc.GetDevice("D0", out readData); // Đọc dữ liệu từ thiết bị PLC, ví dụ từ địa chỉ "D100"
+            if (readResult == 0)
+            {
+                txtData.Text = readData.ToString(); // Hiển thị dữ liệu đọc được trong ô văn bản
+                MessageBox.Show("Read Success!", "Message", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                MessageBox.Show("Read Error!", "Message", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnWrite_Click(object sender, EventArgs e)
+        {
+            if (!IsConnected()) // Kiểm tra kết nối trước khi ghi dữ liệu
+            {
+                MessageBox.Show("PLC is not connected", "Message", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            int writeData;
+            if (int.TryParse(txtData.Text, out writeData))
+            {
+                int writeResult = plc.SetDevice("D10", writeData); // Ghi dữ liệu vào thiết bị PLC, ví dụ vào địa chỉ "D100"
+                if (writeResult == 0)
+                {
+                    MessageBox.Show("Write Success!", "Message", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                else
+                {
+                    MessageBox.Show("Write Error!", "Message", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Invalid Data!", "Message", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+        private void ReadFloatBlock(string deviceName, int length)
+        {
+            if (!IsConnected())
+            {
+                MessageBox.Show("PLC is not connected", "Message", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            short[] buffer = new short[length * 2]; // Mỗi số thực (float) chiếm 2 ô nhớ dạng short
+            int result = plc.ReadDeviceBlock2(deviceName, length * 2, out buffer[0]); // Đọc dữ liệu dạng short
+
+            if (result == 0)
+            {
+                float[] floatData = new float[length];
+                for (int i = 0; i < length; i++)
+                {
+                    int high = buffer[i * 2]; // Phần high của float
+                    int low = buffer[i * 2 + 1]; // Phần low của float
+                    floatData[i] = ConvertToFloat(high, low); // Chuyển đổi 2 ô nhớ thành 1 giá trị float
+                }
+
+                // Hiển thị dữ liệu float (ví dụ: trong TextBox hoặc MessageBox)
+                txtData.Text = string.Join(", ", floatData);
+                MessageBox.Show("Read Float Success!", "Message", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                MessageBox.Show("Read Float Error!", "Message", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void WriteFloatBlock(string deviceName, float[] values)
+        {
+            if (!IsConnected())
+            {
+                MessageBox.Show("PLC is not connected", "Message", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            short[] buffer = new short[values.Length * 2]; // Mỗi số thực (float) chiếm 2 ô nhớ dạng short
+            for (int i = 0; i < values.Length; i++)
+            {
+                int[] intData = ConvertFromFloat(values[i]); // Chuyển đổi float thành 2 ô nhớ (int)
+                buffer[i * 2] = (short)intData[0]; // Chuyển đổi phần thứ nhất sang short
+                buffer[i * 2 + 1] = (short)intData[1]; // Chuyển đổi phần thứ hai sang short
+            }
+
+            int result = plc.WriteDeviceBlock2(deviceName, values.Length * 2, ref buffer[0]); // Gọi phương thức với buffer kiểu short
+
+            if (result == 0)
+            {
+                MessageBox.Show("Write Float Success!", "Message", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                MessageBox.Show("Write Float Error!", "Message", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+        private float ConvertToFloat(int high, int low)
+        {
+            byte[] bytes = new byte[4];
+            BitConverter.GetBytes((short)high).CopyTo(bytes, 0); // Chuyển phần high thành byte[]
+            BitConverter.GetBytes((short)low).CopyTo(bytes, 2); // Chuyển phần low thành byte[]
+            return BitConverter.ToSingle(bytes, 0); // Chuyển byte[] thành float
+        }
+
+
+        private int[] ConvertFromFloat(float value)
+        {
+            byte[] bytes = BitConverter.GetBytes(value);
+            int lowWord = bytes[0] | (bytes[1] << 8);
+            int highWord = bytes[2] | (bytes[3] << 8);
+            return new int[] { lowWord, highWord };
+        }
+
+        private void btnReadFloat_Click(object sender, EventArgs e)
+        {
+            ReadFloatBlock("D4", 2);
+        }
+
+        private void btnWriteFloat_Click(object sender, EventArgs e)
+        {
+            if (!IsConnected()) // Kiểm tra kết nối trước khi ghi dữ liệu
+            {
+                MessageBox.Show("PLC is not connected", "Message", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            // Ghi giá trị float 30.3 vào D100
+            WriteFloatBlock("D16", new float[] { 12.2f });
+        }
+    }
+}
